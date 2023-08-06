4fa5b3e3542fe80d79f30d3e0946ab77c50a46a7
JonuzNuhi <jnuhi690@gmail.com>
2023-08-06 19:41:07 +0200

    Implemented JPA Visit Service, Added Loader Data. Closes #47

--- pet-clinic-data/src/main/java/com/example/petclinic/services/map/VisitMapService.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/map/VisitMapService.java
@@ -2,9 +2,11 @@ package com.example.petclinic.services.map;
 
 import com.example.petclinic.model.Visit;
 import com.example.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
@@ -23,7 +25,7 @@ public class VisitMapService extends AbstractMapService<Visit, Long> implements 
                 || visit.getPet().getOwner().getId() == null){
             throw new RuntimeException("Invalid Visit");
         }
-        return save(visit);
+        return super.save(visit);
     }
 
     @Override
