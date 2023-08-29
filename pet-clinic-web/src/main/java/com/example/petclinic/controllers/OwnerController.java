3138d06cbd7efd19ad197406704edd7deac4b9e7
JonuzNuhi <jnuhi690@gmail.com>
2023-08-29 14:24:59 +0200

    Added display Owner. Closes #53

--- pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/petclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.example.petclinic.controllers;
 import com.example.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class OwnerController {
@@ -24,6 +27,13 @@ public class OwnerController {
     public String findOwners(){
 
         return "notimplemented";
+    }
+
+    @GetMapping("/owners/{ownerId}")
+    public ModelAndView showOwner(@PathVariable Long ownerId) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(ownerId));
+        return mav;
     }
 
 }
